214f7f30bd3d259f5d05d31fd1d4303c9faf9ed1
devk27 <98809450+devk27@users.noreply.github.com>
2024-02-12 22:06:59 +0530

    Add files via upload
    Handling TK_ID and TK_FIELDID

---
+++ identifier.c
@@ -0,0 +1,50 @@
+typedef struct TOKEN{} TOKEN;
+int state;
+
+int digit_2to7(char c){
+    int num = c - '0';
+    if(num >= 2 && num <= 7)return 1;
+    else return 0;
+} 
+
+int lowercase(char c){
+    int num = c - 'a';
+    if(num >= 0 && num <= 25)return 1;
+    else return 0;
+}
+
+// handling TK_ID and TK_FIELDID
+TOKEN get_token(){
+    TOKEN mytoken;
+    char c;
+    while(1){
+        switch(state){
+            case 12:    c = nextchar();
+                        if(digit_2to7(c)) state = 13;
+                        else if(lowercase(c)) state = 16;
+                        else state = 17;
+                        break;
+            case 13:    c = nextchar();
+                        if(c=='b' || c=='c' || c=='d') state = 13;
+                        else if(digit_2to7(c)) state = 14;
+                        else state = 15;
+                        break;
+            case 14:    c = nextchar();
+                        if(digit_2to7(c)) state = 14;
+                        else state = 15;
+                        break;
+            case 15:    retract(1); //TK_ID
+                        mytoken.token = search_token();
+                        if(mytoken.token == IDENTIFIER) mytoken.value = get_id_string();
+                        return mytoken;
+            case 16:    c = nextchar();
+                        if(lowercase(c)) state = 16;
+                        else state = 17;
+                        break;
+            case 17:    retract(1); //TK_FIELDID
+                        mytoken.token = search_token();
+                        if(mytoken.token == IDENTIFIER) mytoken.value = get_id_string();
+                        return mytoken;           
+        }
+    }
+}
