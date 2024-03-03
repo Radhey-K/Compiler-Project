8a40ed0f2a53ed533e1e8088ca1adaabd80c27ad
Aaryan Garg <aaryan.garg1@gmail.com>
2024-03-03 18:51:47 +0530

    ft : parse tree generation using stack

--- stack.c
+++ stack.c
@@ -2,10 +2,11 @@
 #include "stack.h"
 // Creating stack using linked list of symbol type.
 // Function to create a new stack node
-struct StackNode *createNode(symbol data)
+struct StackNode *createNode(symbol data, Node * treeNode)
 {
     struct StackNode *stackNode = (struct StackNode *)malloc(sizeof(struct StackNode));
     stackNode->data = data;
+    stackNode->treeNode = treeNode;
     stackNode->next = NULL;
     return stackNode;
 }
@@ -18,91 +19,96 @@ int isEmpty(struct StackNode *stack)
 }
 
 // Function to push a symbol onto the stack
-void push(struct StackNode **stack, symbol data)
+void push(struct StackNode **stack, symbol data, Node * treeNode)
 {
-    struct StackNode *stackNode = createNode(data);
+    struct StackNode *stackNode = createNode(data, treeNode);
     stackNode->next = *stack;
     *stack = stackNode;
-    printf("Symbol pushed to stack\n");
 }
 
 // Function to pop a symbol from the stack. If empty, returning a default symbol with isterminal=-1 to indicate error
-symbol pop(struct StackNode **stack)
+
+//If not empty return StructNode (we need both pointer + symbol for lookup) else return NULL pointer
+struct StackNode * pop(struct StackNode **stack)
 {
     if (isEmpty(*stack))
     {
-        printf("Stack is empty\n");
-        // Return a default symbol if the stack is empty
-        symbol default_symbol;
-        default_symbol.is_terminal = -1;
-        return default_symbol;
+        // printf("Stack is empty\n");
+        // // Return a default symbol if the stack is empty
+        // symbol default_symbol;
+        // default_symbol.is_terminal = -1;
+        // return default_symbol;
+        return NULL;
     }
     struct StackNode *temp = *stack;
     *stack = (*stack)->next;
-    symbol popped_symbol = temp->data;
-    free(temp);
-    return popped_symbol;
+    // symbol popped_symbol = temp->data;
+    return temp;
+    // free(temp);
+    // return popped_symbol;
 }
 
 // Function to see top of the top symbol of the stack. If empty, returning a default symbol with isterminal=-1 to indicate error
-symbol top(struct StackNode *stack)
+struct StackNode * top(struct StackNode *stack)
 {
     if (isEmpty(stack))
     {
-        printf("Stack is empty\n");
-        // Return a default symbol if the stack is empty
-        symbol default_symbol;
-        default_symbol.is_terminal = -1;
-        return default_symbol;
+        // printf("Stack is empty\n");
+        // // Return a default symbol if the stack is empty
+        // symbol default_symbol;
+        // default_symbol.is_terminal = -1;
+        // return default_symbol;
+        return NULL;
     }
-    return stack->data;
+    return stack;
+    // return stack->data;
 }
 
 // Demo function
 // Outputting the token name as a integer, not as a string.
 int main()
 {
-    struct StackNode *stack = NULL;
-    symbol symbol1 = {.t = TK_ERROR, .is_terminal = 1};
-    push(&stack, symbol1);
-    printf("Top symbol: ");
+    // struct StackNode *stack = NULL;
+    // symbol symbol1 = {.t = TK_ERROR, .is_terminal = 1};
+    // push(&stack, symbol1);
+    // printf("Top symbol: ");
 
-    // Top() after popping
-    symbol top_symbol = top(stack);
-    if (top_symbol.is_terminal)
-        printf("Terminal: tokName %d\n", top_symbol.t);
-    else
-        printf("Non-terminal: nonterminal %d\n", top_symbol.nt);
+    // // Top() after popping
+    // struct StackNode * top_symbol = top(stack);
+    // if (top_symbol->data)
+    //     printf("Terminal: tokName %d\n", top_symbol.t);
+    // else
+    //     printf("Non-terminal: nonterminal %d\n", top_symbol.nt);
 
-    symbol symbol2 = {.nt = booleanExpression, .is_terminal = 0};
-    push(&stack, symbol2);
+    // symbol symbol2 = {.nt = booleanExpression, .is_terminal = 0};
+    // push(&stack, symbol2);
 
-    // Top() again after pushing
-    printf("Top symbol after pop: ");
-    top_symbol = top(stack);
-    if (top_symbol.is_terminal)
-        printf("Terminal: tokName %d\n", top_symbol.t);
-    else
-        printf("Non-terminal: nonterminal %d\n", top_symbol.nt);
+    // // Top() again after pushing
+    // printf("Top symbol after pop: ");
+    // top_symbol = top(stack);
+    // if (top_symbol.is_terminal)
+    //     printf("Terminal: tokName %d\n", top_symbol.t);
+    // else
+    //     printf("Non-terminal: nonterminal %d\n", top_symbol.nt);
 
-    symbol symbol3 = {.t = TK_NOT, .is_terminal = 1};
-    push(&stack, symbol3);
+    // symbol symbol3 = {.t = TK_NOT, .is_terminal = 1};
+    // push(&stack, symbol3);
 
-    // Pop a symbol from the stack
-    printf("Popped symbol: ");
-    symbol popped_symbol = pop(&stack);
-    if (popped_symbol.is_terminal)
-        printf("Terminal: tokName %d\n", popped_symbol.t);
-    else
-        printf("Non-terminal: nonterminal %d\n", popped_symbol.nt);
+    // // Pop a symbol from the stack
+    // printf("Popped symbol: ");
+    // symbol popped_symbol = pop(&stack);
+    // if (popped_symbol.is_terminal)
+    //     printf("Terminal: tokName %d\n", popped_symbol.t);
+    // else
+    //     printf("Non-terminal: nonterminal %d\n", popped_symbol.nt);
 
-    // Top() again after popping
-    printf("Top symbol after pop: ");
-    top_symbol = top(stack);
-    if (top_symbol.is_terminal)
-        printf("Terminal: tokName %d\n", top_symbol.t);
-    else
-        printf("Non-terminal: nonterminal %d\n", top_symbol.nt);
+    // // Top() again after popping
+    // printf("Top symbol after pop: ");
+    // top_symbol = top(stack);
+    // if (top_symbol.is_terminal)
+    //     printf("Terminal: tokName %d\n", top_symbol.t);
+    // else
+    //     printf("Non-terminal: nonterminal %d\n", top_symbol.nt);
 
-    return 0;
+    // return 0;
 }
